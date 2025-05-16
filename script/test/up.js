66f61e7a6cd97cf98f539f1f550fb84c9cea12b5
ksu-x <post2les@gmail.com>
2025-05-16 17:01:13 +0800

    Update up.js

--- script/test/up.js
+++ script/test/up.js
@@ -1,34 +1,12 @@
-// @name Shadowrocket Logger
-// @version 1.0
-// @description Report each request to external server
-// @author ChatGPT
-// @type http-request
-// @pattern ^https?://.*
-// @script
+// report-logger.js
+(async function () {
+  const reportUrl = $arguments['report_url'];
 
-(async function() {
-  const url = $request.url;
-  const method = $request.method;
-  const headers = $request.headers;
-  const hostname = $request.hostname;
+  const encodedUrl = encodeURIComponent($request.url);
+  const fullUrl = `${reportUrl}?url=${encodedUrl}`;
 
-  // 拼接完整地址（你可以只上传 host，也可以带 path）
-  const reportData = {
-    method,
-    url,
-    hostname,
-    time: new Date().toISOString()
-  };
-
-  // 你自己的接收服务器地址（支持 GET/POST）
-  const reportUrl = $arguments['report_url']
-
-  // 用 POST 上报（推荐）
-  await $httpClient.post(reportUrl, {
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(reportData)
+  // 发 GET 请求到服务器
+  await $httpClient.get(fullUrl, () => {
+    $done({});
   });
-
-  // ✅ 允许原始请求继续
-  $done({});
 })();
